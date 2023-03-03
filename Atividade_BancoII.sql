fea77830618e5a486ea6cc759cc518d30ecbad97
João Marcos <112454993+joaomarcospsza@users.noreply.github.com>
2023-03-03 15:24:42 -0300

    Alteração
    Alterando os comentários do código.

--- Atividade_BancoII.sql
+++ Atividade_BancoII.sql
@@ -1,7 +1,8 @@
-DROP DATABASE IF EXISTS atividade02;
-CREATE DATABASE atividade02;
+DROP DATABASE IF EXISTS atividade02; -- DDL, comando para remoção
+CREATE DATABASE atividade02; -- DDL, comando de definição
 USE atividade02;
 
+-- Criando a TABELA clientes - DDL, comando de definição
 CREATE TABLE clientes(
 id_cliente INT NOT NULL,
 nome VARCHAR(255) NOT NULL,
@@ -9,6 +10,7 @@ whatsapp VARCHAR(255) NOT NULL,
 CONSTRAINT cliente_PK PRIMARY KEY (id_cliente)
 );
 
+-- Criando a TABELA servicos - DDL, comando de definição
 CREATE TABLE servicos(
 id_servicos INT NOT NULL,
 nome_servico VARCHAR(255) NOT NULL,
@@ -16,6 +18,7 @@ valor_servico DECIMAL(6,2) NOT NULL,
 CONSTRAINT servico_PK PRIMARY KEY(id_servico)
 );
 
+-- Criando a TABELA servico_por_cliente - DDL, comando de definição
 CREATE TABLE servico_por_cliente(
 id_servico_cliente INT NOT NULL,
 id_cliente INT NOT NULL,
@@ -25,16 +28,17 @@ CONSTRAINT id_cliente_FK FOREIGN KEY (id_cliente) REFERENCES clientes (id_client
 CONSTRAINT id_servico_FK FOREIGN KEY (id_servicos) REFERENCES servicos (id_servicos)
 );
 
--- INSERINDO DADOS NA TABELA CLIENTE - DML, comandos de armazenamento 
+-- Inserindo DADOS na TABELA clientes - DML, comandos de armazenamento 
 INSERT INTO clientes (nome, whatsapp) VALUES ('João Marcos',   '44856963412');
 INSERT INTO clientes (nome, whatsapp) VALUES ('Luis Felipe',   '44781334989');
 INSERT INTO clientes (nome, whatsapp) VALUES ('Gustavo Liu',   '44965875321');
 INSERT INTO clientes (nome, whatsapp) VALUES ('Matheus Ramos', '44586523145');
 INSERT INTO clientes (nome, whatsapp) VALUES ('Cauê Grande',   '44758521232');
 
--- INSERINDO DADOS NA TABELA SERVIÇOS - DML, comandos de armazenamento 
+-- Inserindo DADOS na TABELA servicos - DML, comandos de armazenamento 
 INSERT INTO sevicos (nome_servico, valor_servico) VALUES ('Limpeza interna NoteBook', 50.00);
 INSERT INTO sevicos (nome_servico, valor_servico) VALUES ('Formatação do Computador', 100.00);
 INSERT INTO sevicos (nome_servico, valor_servico) VALUES ('Troca de peça queimada', 350.00);
 INSERT INTO sevicos (nome_servico, valor_servico) VALUES ('Instalação de Internet', 150.00);
 INSERT INTO sevicos (nome_servico, valor_servico) VALUES ('Montagem do Computador',  85.00);
+
